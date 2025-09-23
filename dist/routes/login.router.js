690975c0571d6739f80348a21616c6ef0eb91f68
david-mendes-s <david.a.mendes.s@gmail.com>
2025-09-22 23:43:59 -0300

    [Deploy] Deploy Build Ajustada

--- dist/routes/login.router.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import LoginController from '../controllers/login.controller';
-const loginRouter = Router();
-loginRouter.post('/', LoginController.createLogin);
-export default loginRouter;
--- dist/routes/refresh.router.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import RefreshController from '../controllers/refresh.controller';
-const router = express.Router();
-router.post('/refresh-token', RefreshController.refreshAccessToken);
-export default router;
